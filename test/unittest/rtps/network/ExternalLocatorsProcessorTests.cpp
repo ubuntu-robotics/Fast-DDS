bdfc3d31544f4a847eecc3226e4668a9ce9d0c96
Miguel Company <MiguelCompany@eprosima.com>
2022-08-05 08:23:28 +0200

    TEMP
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- test/unittest/rtps/network/ExternalLocatorsProcessorTests.cpp
+++ test/unittest/rtps/network/ExternalLocatorsProcessorTests.cpp
@@ -14,6 +14,48 @@
 
 #include <gtest/gtest.h>
 
+#include <fastdds/rtps/common/LocatorWithMask.hpp>
+#include <fastrtps/utils/IPLocator.h>
+
+#include <rtps/network/ExternalLocatorsProcessor.hpp>
+
+using namespace eprosima::fastdds::rtps;
+using namespace eprosima::fastrtps::rtps;
+
+struct ExternalAddress
+{
+    ExternalAddress(
+            const std::string& address,
+            uint8_t mask,
+            uint32_t user_port,
+            uint32_t metatraffic_port)
+    {
+        user.kind = LOCATOR_KIND_UDPv4;
+        user.port = user_port;
+        user.mask(mask);
+        IPLocator::setIPv4(user, address);
+
+        metatraffic = user;
+        metatraffic.port = metatraffic_port;
+    }
+
+    LocatorWithMask user;
+    LocatorWithMask metatraffic;
+};
+
+struct BasicNodeConfig
+{
+    // User and metatraffic locators for externality 2
+    ExternalAddress public_ip_locators;
+    ExternalAddress machine_ip_locators;
+    ExternalAddress container_ip_locators;
+};
+
+TEST(ExternalLocatorsProcessorTests, add_external_locators_participant)
+{
+
+}
+
 int main(
         int argc,
         char** argv)
